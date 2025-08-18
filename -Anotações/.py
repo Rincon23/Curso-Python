8125be7c791be53f97e66c4ca35dbcb1e6fe48ca
Rincon23 <enzorincon2003@gmail.com>
2025-08-18 17:09:19 -0300

    Aula 1 - Aula 6

---
+++ -Anotações/.py
@@ -0,0 +1,29 @@
+# Variaveis 
+nome = 'Enzo'
+idade = 21
+peso = 80
+print(nome,idade,peso)
+
+#Tipos de váriaveis
+int 7 -4 0
+float 4.5 0.03 -15.2345 7.0
+bool True False
+str (String) 'olá' '7.5' ''
+
+#para testar o tipo
+print(type(n))
+n = input('Digite algo ')
+print(n.isnumeric())
+n.isalpha()
+n.isalnum()
+n.isupper()
+n.islower()
+
+#.format
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+
+print('A soma entre {} e {} vale {}'.format(n1,n2,s))
+
+
