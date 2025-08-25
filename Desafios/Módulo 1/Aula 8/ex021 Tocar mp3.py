597b8c4c8afeac26b2facd244f1e766ff6339d7c
Rincon23 <enzorincon2003@gmail.com>
2025-08-25 15:29:40 -0300

    Organizando em Aulas

--- Desafios/Módulo 1/Aula 8/ex021 Tocar mp3.py
+++ Desafios/Módulo 1/Aula 8/ex021 Tocar mp3.py
@@ -1,7 +1,7 @@
 import pygame
 
 pygame.init()
-pygame.mixer.music.load("Desafios\Arquivos\ex021\kids-saying-yay-sound-effect_3.mp3")
+pygame.mixer.music.load("Desafios\Módulo 1\Aula 8\Arquivos\ex021\kids-saying-yay-sound-effect_3.mp3")
 pygame.mixer.music.play(loops=0)
 
 while pygame.mixer.music.get_busy():
